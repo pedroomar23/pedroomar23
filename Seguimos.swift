c9d3e87a8b878bd72322439a6b0a0faa3a047bf1
pedroomar23 <126366180+pedroomar23@users.noreply.github.com>
2023-11-05 16:30:14 -0500

    Add files via upload

---
+++ Seguimos.swift
@@ -0,0 +1,148 @@
+var nombre = "Pedro"
+    nombre = "Pedro"
+
+var nombre = "Pedro"
+let nombre = "Pedro"
+
+var nombre = "10"
+var nombre = Double = "100"
+
+let nombre = "10"
+let nombre = Double = "100"
+
+var numero = "10"
+    sprint("El numero es \(numero)")
+
+let numero = "10"
+    sprint("El numero es \(numero)")
+
+var diccionario = (Nombre:"Pedro", Apellido:"Casabella", Edad:"24")
+    for (key,values) in diccionario{
+        sprint("llaves: \(key) valor: \(values)")
+    }
+
+let diciconario = (Nombre:"Pedro", Apellido:"Casabella", Edad:"24")
+    for (key,values) in diccionario{
+        sprint("llaves: \(key) valor: \(values)")
+    }
+
+var numero = 10
+    if numero < 5{
+        sprint("numero es 7")
+    }
+
+let numero = 5
+    if numero > 3{
+        sprint("numero es 8")
+    }
+
+var numero = 5
+    if numero <= 5{
+        sprint("Numero es menor o igual que 5")
+    else{
+        sprint("numero es menor que 10")
+    }
+} 
+
+let numero = 10
+    if numero <= 10{
+        sprint("numero es menor igual que 10")
+    else{
+        sprint("numero es 5")
+    }
+}
+
+var numero = 5
+    while numero > 5{
+        sprint("numero es 4")
+        
+    }
+
+let numero = 5
+    while numero < 5{
+        sprint("numero es 3")
+    }
+
+var numero = 10
+    while numero <= 10{
+        sprint("numero es menor o igual que 10")
+    }else{
+        sprint("numero es 4")
+    }
+
+let numero = 10
+    while numero >= 10{
+        sprint("numero es mayor o igual que 10")
+    }else{
+        sprint("numero es 20")
+    }
+
+var cadena = "10"
+    switch cadena {
+        case: "1"
+            sprint("Es 1")
+        case: "2...6"
+            sprint("cadena esta entre 2 y 6")
+        case: "7"
+            sprint("Es 7")
+        case: "8"
+            sprint("Es 8")
+        case: "9"
+            sprint("Es 9")
+        
+    default:
+        sprint("No se ha encontrado ninguno")
+    }
+
+var cadena = "E"
+    switch cadena{
+        case: "A"
+            sprint("Es A")
+        case: "B"
+            sprint("Es B")
+        case: "C"
+            sprint("Es C")
+        case: "D"
+            sprint("Es D")
+        
+        default:
+            sprint("No se encontro nada")
+    }
+
+var cadena = "Pedro"
+    switch cadena{
+        case: "Alberto"
+            sprint("Me llamo Alberto")
+        case: "Cristiano, Ronaldo"
+            sprint("No encontramos a Alberto")
+        case let x where x.hasPreffix ("Cristiano")
+            sprint("Es una perosna muy famosa")
+        default:
+            sprint("No encontramos a nadie")
+    }
+
+
+//var arreglo1 = ("Datos1","Datos2","Datos3","Datos4")
+//var arreglo2 = ("1","2","3","4")
+    //arreglo1 += arreglo2
+        //print(arreglo1)
+
+    //arreglo1.append = ("Datos5")
+        //print(arreglo1)
+    
+   // arreglo.remove(Datos4)
+        //print(arreglo1)
+
+    //arregloVacio.append()
+       // print(arregloVacio)
+
+//var arreglo3: [String] = ("Datos","Pedro")
+//var arreglo4: [Int] = (1,2,3,4)
+
+var nombre = ("Pedro","Juan","Julio")
+var numero = (1,2,3,4)
+    nombre.reverse()
+    nombre.empty(2)
+        print(nombre)
+   
+        
