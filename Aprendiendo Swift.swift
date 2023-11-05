1f974483675f622fb86d53cae08cc5098996d11b
pedroomar23 <126366180+pedroomar23@users.noreply.github.com>
2023-11-05 16:23:03 -0500

    Esto es una prueba
    Esto es una prueba

---
+++ Aprendiendo Swift.swift
@@ -0,0 +1,81 @@
+var nombre = "Pedro"
+    nombre = "Pedro"
+
+let constante = "20"
+
+var nombre = 100
+var nombre: Double = 100
+
+let nombre = 100
+let nombre: Double = 100
+
+var cadenaA = "Pedro"
+var cadenaB = "Omar"
+
+cadenaC = cadenaA + cadenaB
+
+var cadenaA = "Pedro"
+var cadenaB = "Omar"
+var cadenaC = "10"
+
+var cadenaC = ("El numero es de \(numero)")
+
+/* var cadena = "Pedro"
+   let cadena = "Omar"
+*/
+
+// var cadena = "Pedro"
+// let cadena = "Omar"
+
+var arreglo = ["elemento A", "elementoB", "elementoC", "elementoD"]
+let arregloCons = ["elementoA", "elementoB"]
+
+print(arreglo[1])
+
+arreglo.append("Nuevo Elemento")
+arregloCons.append("elementoB")
+    print(arreglo)
+    print(arreglo.count)
+    arreglo.isEmpty
+
+arreglo[2] = "Pedro"
+    print(arreglo)
+
+arreglo[1...3] = ["Jose", "Juan", "Pedro"]
+    print(arreglo)
+
+arreglo.removeAtIndex(3)
+//arreglo.removeLast()
+
+// let datoborrado = arreglo.removeLast()
+
+//arreglo.removeAll()
+
+var arreglonombre: [String] = ["Jose","MArio","Pedro"]
+var numeros: [Int] = [1,2,3,4]
+var arregloVacio = [String]()
+var arregloVacio = [Int]()
+
+arregloVacio.append("Cadena A")
+arregloVacio.count
+
+arregloVacio += ["Datos X","Datos Y","Datos Z"]
+
+var arreglo1 = ["Datos1","Datos2","Datos3"]
+var arreglo2 = ["a","b","c"]
+    arreglo1 += arreglo2
+        print(arreglo1)
+        
+arreglo1 = arreglo2.reverse()
+
+
+repeat
+
+
+
+
+
+
+
+
+
