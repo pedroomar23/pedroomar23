d0fcd5abc6290a68f11dc7720f04918c4a781862
pedroomar23 <126366180+pedroomar23@users.noreply.github.com>
2023-11-05 16:29:46 -0500

    Add files via upload

---
+++ Prueba.swift
@@ -0,0 +1,54 @@
+//var dato = ("Juan","Julio","Oscar")
+//var dato1 = (1,,2,3,4,)
+    dato += dato1
+        print(dato)
+
+//let arraysCons = ("Pedro","Juan","Oscar")
+    print(arraysCons)
+
+//var dato = ("Juan","Luis","Carlos")
+    dato.append("Oscar")
+        print(dato)
+    dato.remove(2)
+        print(dato)
+    dato.removeAll
+        print(dato)
+
+//var dato: [String] = ("Juan","Pedro","Oscar")
+        print[String]()
+
+//var dato1: [Int] = (1,2,3,4)
+        print[Int]()
+
+//var dato = ("Pedro","Juan","Oscar")
+        dato.removeAtIndex(2)
+            print(dato)
+
+    arraysEmpty += ("Pedro","Juan","Julio")
+
+   var dato = ("Pedro","Juan","Julio")
+   var dato1 = (1,2,3,4)
+        dato.reverse()
+            print(dato)
+
+   // dato.removeLast(1)
+        print(dato)
+
+    //dato[1...3] = ["Juan","Julio",'Pedro']
+        print(dato)
+  
+  //var arraysEmpty = ("Pedro","Oscar","Jose")
+        arraysEmpty.removeAtIndex(2)
+            print(arraysEmpty)
+
+
+
+
+    
+
+
+
+
+
+    
+    
