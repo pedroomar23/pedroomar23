3168105ce63ab23265d9cdd692a1d2f11140c91a
pedroomar23 <126366180+pedroomar23@users.noreply.github.com>
2023-11-05 16:28:54 -0500

    Add files via upload

---
+++ Comenzando Proyecto.swift
@@ -0,0 +1,116 @@
+//let dato = 100
+//let dato: Double = 100.000000000
+
+//var dato = 100
+    dato = 100
+
+//let datoCons = 100
+    datoCons = 100
+
+//let datoCons: Double = 100
+
+let dato: Double = 10
+    print(dato)
+
+let dato = "El ancho es"
+let ancho = 94
+    datoDelAncho = ancho + [String](94)
+        print(dato)
+
+let dato = 20
+let dato = 10
+
+let totalDato = "El dato es \(dato)"
+    print(totalDato)"
+
+let datoTotal = "La suma de \(dato + dato) dato"
+
+let cita = """
+    Cristiano Ronaldo es el mejor jugador del mundo y eso nadie los puede discutir
+
+    "El dato es \(dato)"
+    
+    """
+
+//var dato = ["Pedro","Juan","Pedro"]
+//var dato1 = [1,2,3,4]
+    dato.count()
+        print(dato)
+    dato.removeAtIndex()
+        print(dato)
+    dato.remove(2)
+        print(dato)
+    dato.append("Carlos")
+        print(dato)
+
+    arraysIsEmpty.append("Pedro","Juan","Julio")
+        print(arraysIsEmpty)
+
+    dato += dato1
+        print(dato)
+
+    arraysIsEmpty += ("Pedro","Juan")
+
+//var dato = ["Pedro","Juan","Pedro"]
+//var dato1 = [1,2,3,4]
+    sprint(1)
+
+arrays[1...3] = ("Pedro","Juan","Oscar")
+    sprint(arrays)
+
+var datos = ["Pedro","Juan","Oscar"]
+var datos1 = [1,2,3,4]
+    for (keys,values) in datos{
+        print("llaves: \(keys) valor: \(values)")
+    } 
+
+var ocupaciones = [
+    "Pedro" = "Profesor"
+    "Juam" = "Arquitecto"
+        ]
+
+ocupaciones["Julia"] = "Relaciones Publicas"
+
+frutas.append[moras]
+    print(frutas)
+
+// Imprime "[Peras, Manzanas, Mangos]"
+
+arregloVAcio = []
+ocumaciones = [:]
+
+let arregloVacio: [Stirng] = []
+    print(arregloVacio)
+
+let diccionarioVacio: [String: Float] = [:]
+    print(diccionarioVacio)
+
+
+let dato = 100
+let dato1 = 100.000
+
+let datoCons: Double = 100
+    print(dato)
+
+let dato = 20
+let dato1 = 10
+
+let totalDato = "El total es \(dato)"
+
+let datosIndividuales = [5,10,15,20]
+let datosDelGrupo = [0]
+    for datosIndividuales > 50 {
+        if datosDelgrupo += 3 {
+    } else {
+        datosDelGrupo += 1
+    }
+
+    print(datosDelGrupo)
+
+let datosDelGrupo = if datosIndividuales > 50 {
+    "datosDelGrupo += 20"
+} else { datosDelGrupo = 15
+
+}
+
+print(datosDelGrupo)
